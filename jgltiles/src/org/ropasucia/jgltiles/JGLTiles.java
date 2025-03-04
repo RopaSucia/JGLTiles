9fa531e831608d0b04dafef02b41ecfa2a8aa28f
RopaSucia <129201907+RopaSucia@users.noreply.github.com>
2025-03-04 18:54:12 -0500

    Add files via upload

---
+++ jgltiles/src/org/ropasucia/jgltiles/JGLTiles.java
@@ -0,0 +1,9 @@
+package org.ropasucia.jgltiles;
+
+import org.lwjgl.Version;
+
+public class JGLTiles {
+    public void lwjglVersion() {
+        System.out.println(Version.getVersion());
+    }
+}
